96c15aaff2b396ff880192f931ad8b715a734abe
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2022-02-07 23:58:06 +0100

    refactor/removed the use of native APIs

--- src/UI/components/Features/Characters/CharacterCardList/CharacterCardList.tsx
+++ src/UI/components/Features/Characters/CharacterCardList/CharacterCardList.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
 
 import { motion } from "framer-motion";
 import ScrollToTop from "react-scroll-to-top";
@@ -14,7 +13,7 @@ import "./CharacterCardList.scss";
 import {
   getConcreteCharacters,
   ConcreteCharacter,
-} from "../../../../../services/RickMorty/RickMorty";
+} from "@services/RickMorty/RickMorty";
 
 function CharacterCardList() {
   //state
@@ -28,26 +27,6 @@ function CharacterCardList() {
   const errorHandler = () => {
     setError(null);
   };
-
-  //Portaled components
-
-  const scrollToTop: Element = document.getElementById("scroll-to-top")!;
-  const errorModal: Element = document.getElementById("error-modal")!;
-
-  const scrollToTopUI = ReactDOM.createPortal(
-    <ScrollToTop top={500} smooth color={"#02a9c0"} width="16" height="16" />,
-    scrollToTop
-  );
-
-  const errorUI = ReactDOM.createPortal(
-    <ErrorModal
-      title="Something went wrong ..."
-      errorMessage={`${error} + Check your internet connection and refresh the page
-    `}
-      errorModal={errorHandler}
-    />,
-    errorModal
-  );
 
   useEffect(() => {
     async function loadCharacters() {
@@ -72,53 +51,61 @@ function CharacterCardList() {
   //TODO: Fix this Logic gate hell
   return (
     <>
-      {(error && errorUI) || (isloading && <Spinner />) || (
-        <>
-          {scrollToTopUI}
-          <motion.section
-            initial={{ y: 50 }}
-            animate={{ y: 0 }}
-            transition={{
-              delay: 0.5,
-              x: { type: "spring", stiffness: 50 },
-              default: { duration: 1 },
-            }}
-          >
-            <Pagination
-              updatePage={setPage}
-              pageNumber={page}
-              totalPages={totalPages}
-            />
-          </motion.section>
-          <motion.section
-            className="character-card-grid"
-            animate={{ y: 50 }}
-            transition={{
-              delay: 0.5,
-              x: { type: "spring", stiffness: 100 },
-              default: { duration: 1 },
-            }}
-          >
-            <div className="character-card-grid__container">
-              {characters.map((character) => (
-                <CharacterCard
-                  key={character.id}
-                  name={character.name}
-                  status={character.status}
-                  species={character.species}
-                  type={character.type}
-                  gender={character.gender}
-                  origin={character.origin}
-                  location={character.location}
-                  image={character.image}
-                  episodes={character.episodes}
-                  url={character.url}
-                />
-              ))}
-            </div>
-          </motion.section>
-        </>
-      )}
+      {(error && (
+        <ErrorModal
+          title="Something went wrong ..."
+          errorMessage={`${error} + Check your internet connection and refresh the page
+    `}
+          errorModal={errorHandler}
+        />
+      )) ||
+        (isloading && <Spinner />) || (
+          <>
+            <ScrollToTop top={500} smooth color={"#02a9c0"} width="16" height="16" />
+            <motion.section
+              initial={{ y: 50 }}
+              animate={{ y: 0 }}
+              transition={{
+                delay: 0.5,
+                x: { type: "spring", stiffness: 50 },
+                default: { duration: 1 },
+              }}
+            >
+              <Pagination
+                updatePage={setPage}
+                pageNumber={page}
+                totalPages={totalPages}
+              />
+            </motion.section>
+            <motion.section
+              className="character-card-grid"
+              animate={{ y: 50 }}
+              transition={{
+                delay: 0.5,
+                x: { type: "spring", stiffness: 100 },
+                default: { duration: 1 },
+              }}
+            >
+              <div className="character-card-grid__container">
+                {characters.map((character) => (
+                  <CharacterCard
+                    key={character.id}
+                    name={character.name}
+                    status={character.status}
+                    species={character.species}
+                    type={character.type}
+                    gender={character.gender}
+                    origin={character.origin}
+                    location={character.location}
+                    image={character.image}
+                    episodes={character.episodes}
+                    url={character.url}
+                  />
+                ))}
+              </div>
+            </motion.section>
+          </>
+        )}
     </>
   );
 }
